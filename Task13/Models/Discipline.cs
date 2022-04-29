44532adabad188f89cc95a95b671561bb16924a1
NET-LIO <96789163+NET-LIO@users.noreply.github.com>
2022-04-29 10:36:28 +0300

    Add files via upload

---
+++ Task13/Models/Discipline.cs
@@ -0,0 +1,28 @@
+﻿using LinqToDB.Mapping;
+using System;
+
+namespace Norbit.Crm.Hodeshenas.TaskThirteen
+{
+	/// <summary>
+	/// Дисциплина.
+	/// </summary>
+	public class Discipline
+    {
+		/// <summary>
+		/// Айди дисциплины.
+		/// </summary>
+		[PrimaryKey]
+		public Guid DisciplineId { get; set; }
+
+		/// <summary>
+		/// Название дисциплины.
+		/// </summary>
+		[Column(Name = "Name"), NotNull]
+		public string Name { get; set; }
+
+		public override string ToString()
+		{
+			return $"{DisciplineId} - {Name}";
+		}
+	}
+}
---
+++ Task13/Models/Teacher.cs
@@ -0,0 +1,52 @@
+﻿using LinqToDB.Mapping;
+using System;
+
+namespace Norbit.Crm.Hodeshenas.TaskThirteen
+{
+	/// <summary>
+	/// Учитель.
+	/// </summary>
+    public class Teacher
+    {
+		/// <summary>
+		/// Айди учителя.
+		/// </summary>
+		[PrimaryKey]
+		public Guid TeacherId { get; set; }
+
+		/// <summary>
+		/// Фамилия.
+		/// </summary>
+		[Column(Name = "Surname"), NotNull]
+		public string Surname { get; set; }
+
+		/// <summary>
+		/// Имя.
+		/// </summary>
+		[Column(Name = "Name"), NotNull]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Отчество.
+		/// </summary>
+		[Column(Name = "Patronymic"), NotNull]
+		public string Patronymic { get; set; }
+
+		/// <summary>
+		/// Семестр.
+		/// </summary>
+		[Column(Name = "Semester"), NotNull]
+		public int Semester { get; set; }
+
+		/// <summary>
+		/// Айди дисциплины.
+		/// </summary>
+		[Column(Name = "DisciplineId"), NotNull]
+		public Guid DisciplineId { get; set; }
+
+		public override string ToString()
+		{
+			return $"{TeacherId} - {Surname} - {Name} - {Patronymic} - {Semester} - {DisciplineId}";
+		}
+	}
+}
