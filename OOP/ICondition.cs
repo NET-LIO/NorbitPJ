9a05779af491d2557a87d6e8e6e57112a9c30f0d
NET-LIO <96789163+NET-LIO@users.noreply.github.com>
2022-04-29 10:34:56 +0300

    Add files via upload

---
+++ OOP/ICondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Norbit.Crm.Hodeshenas.TaskEight
+{
+    /// <summary>
+    /// Cостояние машины.
+    /// </summary>
+    interface ICondition
+    {
+        #region Свойства
+
+        /// <summary>
+        /// Перед началом использования машины.
+        /// </summary>
+        string StartEngine();
+
+        #endregion
+    }
+}
