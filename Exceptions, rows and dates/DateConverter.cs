90cd9e1b9b9a99591795dab8f0459c11b0ae6e64
NET-LIO <96789163+NET-LIO@users.noreply.github.com>
2022-04-29 10:35:14 +0300

    Add files via upload

---
+++ Exceptions, rows and dates/DateConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Norbit.Crm.Hodeshenas.TaskNine
+{
+    /// <summary>
+    /// Конвертор даты.
+    /// </summary>
+    internal class DateConverter
+    {
+        /// <summary>
+        /// Конвертирует дату в формате UTC в формат местного времени.
+        /// </summary>
+        /// <param name="utcDate">Дата в формате UTC.</param>
+        /// <returns>Дата в формате местного времени..</returns>
+        public static DateTime ConvertUtcToLocal(DateTime utcDate)
+        {
+            try
+            {
+                return TimeZoneInfo.ConvertTimeFromUtc(utcDate, TimeZoneInfo.Local);
+            }
+            catch (ArgumentException)
+            {
+                throw new ArgumentException("Это не UTC формат!", nameof(utcDate));
+            }
+        }
+
+        /// <summary>
+        /// Конвертирует текст в формат DateTime.
+        /// </summary>
+        /// <param name="text">Входной текст</param>
+        /// <returns>Дата в формате DateTime</returns>
+        public DateTime ConvertToDateTime(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentNullException(nameof(text), "Пустая строка или ссылка.");
+            }
+            try
+            {
+                return Convert.ToDateTime(text);
+            }
+            catch (FormatException e)
+            {
+                throw new FormatException("Формат строки не соответствует дате.", e);
+            }
+        }
+    }
+}
---
+++ Exceptions, rows and dates/ExceptionDivideByZero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Norbit.Crm.Hodeshenas.TaskNine
+{
+    /// <summary>
+    /// Вызывает исключение DivideByZeroException.
+    /// </summary>
+    internal class ExceptionDivideByZero
+    {
+        /// <summary>
+        /// На что будем делить.
+        /// </summary>
+        private int x = 0;
+
+        /// <summary>
+        /// Вызывает исключение.
+        /// </summary>
+        /// <param name="y">Входной параметр</param>
+        public void ThrowException(int y)
+        {
+            try
+            {
+                int getException = y / x;
+            }
+            catch(DivideByZeroException e)
+            {
+                throw new DivideByZeroException("На 0 делить нельзя!", e);
+            }
+        }
+    }
+}
