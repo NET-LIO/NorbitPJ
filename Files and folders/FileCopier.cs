dfa3c46655123d7fe3589d80c31c1d0e5b64dd0d
NET-LIO <96789163+NET-LIO@users.noreply.github.com>
2022-04-29 10:48:49 +0300

    Add files via upload

---
+++ Files and folders/FileCopier.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+
+namespace Norbit.Crm.Hodeshenas.TaskTen
+{
+    /// <summary>
+    /// Побайтовое копирование.
+    /// </summary>
+    public class FileCopier
+    {
+        #region События
+
+        /// <summary>
+        /// Начало копирования.
+        /// </summary>
+        public event EventHandler CopyStart;
+
+        /// <summary>
+        /// Прогресс в процентах.
+        /// </summary>
+        public event EventHandler<int> CopyInProgress;
+
+        /// <summary>
+        /// Конец копирования.
+        /// </summary>
+        public event EventHandler CopyFinish;
+
+        /// <summary>
+        /// Вызов события CopyStart.
+        /// </summary>
+        protected virtual void OnCopyStart()
+        {
+            CopyStart?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Вызов события CopyInProgress.
+        /// </summary>
+        /// <param name="progress">Прогресс</param>
+        protected virtual void OnCopyInProgress(int progress)
+        {
+            CopyInProgress?.Invoke(this, progress);
+        }
+
+        /// <summary>
+        /// Вызов события CopyFinish.
+        /// </summary>
+        protected virtual void OnCopyFinished()
+        {
+            CopyFinish?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Проверка на наличие пути к файлу и размера блока.
+        /// </summary>
+        /// <param name="source">Путь копируемого файла</param>
+        /// <param name="destination">Путь конечного файла</param>
+        /// <param name="blockSize">Размер блока</param>
+        void ParameterCheck(string source, string destination, int blockSize)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentException("Не задан путь копируемого файла");
+            }
+            if (string.IsNullOrEmpty(destination))
+            {
+                throw new ArgumentException("Не задан путь конечного файла");
+            }
+            if (blockSize < 1)
+            {
+                throw new ArgumentException("Размер блока не может быть отрицательным или равен 0!");
+            }
+        }
+
+        /// <summary>
+        /// Побайтовое копирование данных.
+        /// </summary>
+        /// <param name="source">Путь копируемого файда</param>
+        /// <param name="destination">Путь конечного файла</param>
+        /// <param name="blockSize">Размер блока</param>
+        public void Copy(string source, string destination, int blockSize)
+        {
+            ParameterCheck(source, destination, blockSize);
+
+            using (var sourceFile = new FileStream(source, FileMode.Open))
+            {
+                using (var objectiveFile = new FileStream(destination, FileMode.Create))
+                {
+                    OnCopyStart();
+
+                    var readBlock = new byte[blockSize];
+                    var sourceFileLength = sourceFile.Length;
+                    var iteration = 0;
+                    var bytesRead = 0;
+
+                    while (sourceFile.Position < sourceFile.Length)
+                    {
+                        bytesRead = sourceFile.Read(readBlock, 0, blockSize);
+                        objectiveFile.Write(readBlock, 0, bytesRead);
+
+                        iteration++;
+
+                        OnCopyInProgress((int)(sourceFile.Position * 100 / sourceFileLength));
+                    }
+                }
+            }
+
+            OnCopyFinished();
+        }
+
+        #endregion
+    }
+}
---
+++ Files and folders/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Norbit.Crm.Hodeshenas.TaskTen
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                var path = Directory.GetCurrentDirectory();
+                var filePath = $"{path}\\file.txt";
+
+                ReadFile(filePath);
+                CopyFile(filePath, path);
+                WriteToFile(filePath, path);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                PrintConsole(ex);
+            }
+        }
+
+        static void CopyFile(string filePath, string path)
+        {            
+            var objective = $@"{path}\fileCopy.txt";
+
+            var fileCopier = new FileCopier();
+
+            fileCopier.CopyStart += CopyStart;
+            fileCopier.CopyInProgress += CopyInProgress;
+            fileCopier.CopyFinish += CopyFinish;
+
+            try
+            {
+                fileCopier.Copy(filePath, objective, 1);
+            }
+            catch (IOException ex)
+            {
+                throw new IOException(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+
+            fileCopier.CopyStart -= CopyStart;
+            fileCopier.CopyInProgress -= CopyInProgress;
+            fileCopier.CopyFinish -= CopyFinish;
+        }
+
+        static void ReadFile(string filePath)
+        {
+            var fileContents = string.Empty;
+            var stringBuilder = new StringBuilder(fileContents);
+
+            try
+            {
+                stringBuilder.Append($"Содержимое файла: " +
+                    $"\n{File.ReadAllText(filePath)}\n" +
+                    $"\nСодержимое в байтах:\n");
+
+                var allByte = File.ReadAllBytes(filePath);
+
+                foreach (var b in allByte)
+                {
+                    stringBuilder.Append($"0x{b:X} ");
+                }
+
+                Console.WriteLine(stringBuilder);
+            }
+            catch(ArgumentException ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+
+        }
+
+        static void WriteToFile(string filePath, string path)
+        {
+            var fileToWrite = $@"{path}\fileWriteEnd.txt";
+
+            try
+            {
+                string text = "\nМы в конце;)";
+
+                using (var streamWriter = new StreamWriter(fileToWrite))
+                {
+                    Console.WriteLine("\nНачало записи в конец нового файла...");
+
+                    streamWriter.WriteLine(Encoding.UTF8.GetString(File.ReadAllBytes(filePath)) + text);
+                }
+
+                Console.WriteLine($"Готово! Файл сохранен по пути {fileToWrite}");
+            }
+            catch (IOException ex)
+            {
+                throw new IOException(ex.Message);
+            }
+        }
+
+        static void CopyStart(object sender, EventArgs args)
+        {
+            Console.WriteLine("\nКопирование запущено...");
+        }
+
+        static void CopyInProgress(object sender, int arg)
+        {
+            Console.WriteLine($"Прогресс {arg}%");
+        }
+
+        static void CopyFinish(object sender, EventArgs args)
+        {
+            Console.WriteLine("\nКопирование завершено");
+        }
+
+        static void PrintConsole(Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex.Message);
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
