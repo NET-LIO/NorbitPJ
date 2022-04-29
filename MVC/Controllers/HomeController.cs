b8269c3430afb099e556138ca6999df1e19b268b
NET-LIO <96789163+NET-LIO@users.noreply.github.com>
2022-04-29 10:43:00 +0300

    Add files via upload

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Norbit.Crm.Hodeshenas.FinalTaskCS
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Возвращает начальную страницу.
+        /// </summary>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Возвращает страницу с ответом Web сервиса, вызывая метод через C#.
+        /// </summary>
+        /// <returns>Страница с ответом Web сервиса</returns>
+        public ActionResult GetLastTradeCSharp()
+        {
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create("https://localhost:44312/api/trade/last");
+
+                request.Credentials = CredentialCache.DefaultCredentials;
+
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (var receiveStream = response.GetResponseStream())
+                    {
+                        var streamReader = new StreamReader(receiveStream, Encoding.UTF8);
+
+                        var trade = JsonConvert.DeserializeObject<Trade>(streamReader.ReadToEnd());
+
+                        ViewBag.Created = trade.Created.ToLocalTime();
+                        ViewBag.Amount = trade.Amount;
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                throw new WebException("Ошибка с доступом к сервису!", ex);
+            }
+
+            return View();
+        }
+
+        /// <summary>
+        /// Возвращает страницу с ответом Web сервиса, вызывая метод через JS.
+        /// </summary>
+        /// <returns>Страница с ответом Web сервиса</returns>
+        public ActionResult GetLastTradeJS()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC/Scripts/GetLastTradeScript.js
@@ -0,0 +1,20 @@
+﻿/**
+ * Получает сумму и дату последней сделки через обращение к Web сервису.
+ */
+$.ajax({
+    type: 'GET',
+    url: 'https://localhost:44312/api/trade/last',
+    dataType: 'json',
+    success: function (trade) {
+        let created = document.getElementById('Created');
+        let amount = document.getElementById('Amount');
+        var utcDate = new Date(trade.Created);
+        var locaDate = new Date(utcDate.getTime() - (utcDate.getTimezoneOffset() * 60 * 1000));
+
+        created.innerHTML = "Последняя дата сделки - " + locaDate.toLocaleString();
+        amount.innerHTML = "Сумма - " + trade.Amount;
+    }, 
+    error: function (result) {
+        alert('Ошибка выполнения');
+    }
+});
