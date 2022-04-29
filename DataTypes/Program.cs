2a8bbb92a40a7e1dd7ada9280ef165d0485eab2b
NET-LIO <96789163+NET-LIO@users.noreply.github.com>
2022-04-29 10:46:41 +0300

    Add files via upload

---
+++ DataTypes/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+
+namespace Norbit.Crm.Hodeshenas.TaskTwo
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// Главная точка входа.
+        /// </summary>
+        static void Main()
+        {
+            while(true)
+            {
+                Console.Clear();
+                GetMenu();
+            }
+        }
+
+        /// <summary>
+        /// Вывод индекса массы тела.
+        /// </summary>
+        static void BodyMassIndex()
+        {
+            int weight;
+            double height;
+            var calculationLibrary = new BmiCalculatorLibrary();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите через 'Enter' свой вес в килограммах и рост в метрах");
+
+                if(!int.TryParse(Console.ReadLine(), out weight) 
+                    || !double.TryParse(Console.ReadLine(), out height))
+                {
+                    Console.WriteLine("Вы ввели не число, введите еще раз");
+                    Console.ReadLine();
+                    continue;
+                }
+                try
+                {
+                    Console.WriteLine($"Индекс массы тела = {calculationLibrary.СalculateBodyMassIndex(weight, height)}");
+                    ForcedGarbageCleanup();
+                    Console.ReadLine();
+                    break;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вывод случайных чисел массива.
+        /// </summary>
+        static void RandomArrayValue()
+        {
+
+            var randomArrayValue = new RandomArrayValueLibrary();
+
+            int[] array = new int[17];
+            array = randomArrayValue.CreateRandArray(array);
+
+            Console.WriteLine($"Массив - {randomArrayValue.GetArrayAsString(array)}");
+
+            Console.WriteLine($"Минимальное значение массива = {randomArrayValue.GetMinArrayValue(array)}");
+
+            Console.WriteLine($"Максимальное значение массива = {randomArrayValue.GetMaxArrayValue(array)}");
+
+            array = randomArrayValue.SortArrayNumber(array);
+
+            Console.WriteLine($"Отсортированный массив - {randomArrayValue.GetArrayAsString(array)}");
+
+            ForcedGarbageCleanup();
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Вывод среднего числа символов в слове.
+        /// </summary>
+        static void AverageWordLength()
+        {
+            string text;
+            var averageWordLibrary = new AverageWordLibrary();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите строку: ");
+
+                text = Console.ReadLine();
+
+                try
+                {
+                    Console.WriteLine($"Средняя длина слова = {averageWordLibrary.AverageWordLengthOutString(text)}");
+                    ForcedGarbageCleanup();
+                    Console.ReadLine();
+                    break;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вывод и очистка памяти в управляемой куче.
+        /// </summary>
+        static void ForcedGarbageCleanup()
+        {
+            var totalMemory = GC.GetTotalMemory(false);
+
+            Console.WriteLine($"\nОбъем памяти в байтах, которое занято в управляемой куче = {totalMemory}");
+            GC.Collect(1, GCCollectionMode.Optimized);
+
+            GC.Collect(0);
+
+            Console.WriteLine("Объем памяти в байтах: {0}", GC.GetTotalMemory(false));
+        }
+
+        /// <summary>
+        /// Главное меню приложения.
+        /// </summary>
+        static void GetMenu()
+        {
+            Console.WriteLine("Введите номер задания:" +
+                            "\n1.Расчет индекса массы тела." +
+                            "\n2.Генерация случайных чисел в массиве." +
+                            "\n3.Определить среднюю длину слова." +
+                            "\n4.Выйти из программы\n");
+            int taskNumber;
+            if (int.TryParse(Console.ReadLine(), out taskNumber))
+            {
+                try
+                {
+                    switch (taskNumber)
+                    {
+                        case 1:
+                            BodyMassIndex();
+                            break;
+                        case 2:
+                            RandomArrayValue();
+                            break;
+                        case 3:
+                            AverageWordLength();
+                            break;
+                        case 4:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            throw new ArgumentException(message: $"К такому варианту ({taskNumber}) меня мама не готовила!", nameof(taskNumber));
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Ты ввел что-то не то, повтори попытку");
+            }
+        }
+    }
+}
---
+++ DataTypes/RandomArrayValueLibrary.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Norbit.Crm.Hodeshenas.TaskTwo
+{
+    /// <summary>
+    /// Генерация случайных чисел массива.
+    /// </summary>
+    internal class RandomArrayValueLibrary
+    {
+
+        /// <summary>
+        /// Проверка входного массива на наличие значений.
+        /// </summary>
+        /// <param name="array">Задаваемый массив</param>
+        /// <exception cref="ArgumentException">Выполняется в случае выполнения условия</exception>
+        private void ParameterCheck(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array), message: "В задаваемом массиве нету значений!");
+            }
+        }
+
+        /// <summary>
+        /// Возвращает массив, заполненный случайными числами.
+        /// </summary>
+        /// <param name="array">Задаваемый массив</param>
+        /// <returns></returns>
+        public int[] CreateRandArray(int[] array)
+        {
+
+            ParameterCheck(array);
+
+            var rand = new Random();
+
+            for (var i = 0; i < array.Length ; i++)
+            {
+                array[i] = rand.Next();
+            }
+            return array;
+        }
+
+        /// <summary>
+        /// Возвращает записанный массив.
+        /// </summary>
+        /// <param name="array">Задаваемый массив</param>
+        /// <returns></returns>
+        public string GetArrayAsString(int[] array)
+        {
+
+            ParameterCheck(array);
+
+            var result = "";
+
+            for(var i = 0; i < array.Length; i++)
+            {
+                result += $"{array[i]} ";
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Возвращает отсортированный массив.
+        /// </summary>
+        /// <param name="array">Задаваемый массив</param>
+        /// <returns></returns>
+        public int[] SortArrayNumber(int[] array)
+        {
+
+            ParameterCheck(array);
+
+            int tempValue;
+
+            for (var i = 0; i < array.Length - 1 ; i++)
+            {
+                for (var j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        tempValue = array[i];
+                        array[i] = array[j];
+                        array[j] = tempValue;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        /// <summary>
+        /// Возвращает минимальное значение в массиве.
+        /// </summary>
+        /// <param name="array">Задаваемый массив</param>
+        /// <returns>Возвращает минимальное значение</returns>
+        public int GetMinArrayValue(int[] array)
+        {
+            ParameterCheck(array);
+
+            var min = array[0];
+
+            foreach (var value in array)
+            {
+                if (value < min)
+                {
+                    min = value;
+                }
+            }
+
+            return min;
+        }
+
+        /// <summary>
+        /// Возвращает максимальное значение в массиве.
+        /// </summary>
+        /// <param name="arrayGetMin">Задаваемый массив</param>
+        /// <returns>Возвращает максимальное значение</returns>
+        public int GetMaxArrayValue(int[] array)
+        {
+
+            ParameterCheck(array);
+
+            var max = array[0];
+
+            foreach (var value in array)
+            {
+                if (value > max)
+                {
+                    max = value;
+                }
+            }
+
+            return max;
+        }
+    }
+}
