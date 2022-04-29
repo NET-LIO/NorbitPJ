c93639544a9c46b449be19429b78ab73c186fc58
NET-LIO <96789163+NET-LIO@users.noreply.github.com>
2022-04-29 10:37:13 +0300

    Add files via upload

---
+++ TradeLibrary/Trade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Norbit.Crm.Hodeshenas.FinalTaskCS
+{
+    /// <summary>
+    /// Сделка.
+    /// </summary>
+    public class Trade
+    {
+        /// <summary>
+        /// Сумма сделки.
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Дата сделки.
+        /// </summary>
+        public DateTime Created { get; set; }
+
+        public override string ToString()
+        {
+            return $"Дата сделки - {Created.ToLocalTime()} Сумма - {Amount}.\n";
+        }
+    }
+}
